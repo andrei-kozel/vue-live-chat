21538e33ca75cb5d2825efc99f3f63b03af50032
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-06-27 19:46:55 +0200

    Login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="form" @submit.prevent="handleSubmit">
+    <p class="text-3xl mb-10 text-gray-900">Login</p>
+
+    <div class="form-field mb-8">
+      <label class="form-label">Email:</label>
+      <input
+        class="form-input focus:outline-none focus:shadow-outline"
+        type="email"
+        placeholder="Email"
+        v-model="email"
+      />
+    </div>
+    <div class="form-field mb-8">
+      <label class="form-label">Password:</label>
+      <input
+        class="form-input focus:outline-none focus:shadow-outline"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+      />
+    </div>
+    <button class="form-button">Log in</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const email = ref<string>();
+const password = ref<string>();
+
+const handleSubmit = () => {
+  console.log(email.value, password.value);
+};
+</script>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="welcome">
-    <SignupForm />
+    <div v-if="showLogin">
+      <LoginForm />
+      <p class="welcome-hint">
+        No account yet? <span @click="showLogin = false">Signup</span> instead.
+      </p>
+    </div>
+    <div v-else>
+      <SignupForm />
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { ref } from "vue";
 import SignupForm from "@/components/SignupForm.vue";
+import LoginForm from "../components/LoginForm.vue";
 
-export default defineComponent({
-  name: "WelcomeView",
-  components: { SignupForm },
-});
+const showLogin = ref<boolean>(true);
 </script>
 
 <style lang="scss">
 .welcome {
   @apply bg-white p-8 rounded-xl shadow-xl;
+
+  &-hint {
+    @apply text-center mt-5 text-gray-500;
+
+    span {
+      @apply font-bold hover:text-teal-500 cursor-pointer;
+    }
+  }
 }
 </style>
