7cda4b8316011bb08142225d3bfeef47ca4effbc
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-07-03 11:28:26 +0200

    navigation component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navigation">
+    <div>
+      <p>Hey there ... display name here</p>
+      <p class="email">Currently logged in as ... email</p>
+    </div>
+    <button class="button">Logout</button>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.navigation {
+  @apply flex justify-between items-center mb-4;
+
+  p {
+    @apply text-gray-900 text-base;
+  }
+
+  .email {
+    @apply text-sm text-gray-500;
+  }
+
+  .button {
+    @apply bg-teal-300 rounded-full px-4 py-2 font-bold text-white hover:bg-teal-500;
+  }
+}
+</style>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>Chatroom</div>
+  <div class="chat">
+    <NavBar />
+    <div>Chatroom</div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.chat {
+  @apply flex flex-col bg-white p-8 rounded-xl shadow-xl w-full;
+}
+</style>
